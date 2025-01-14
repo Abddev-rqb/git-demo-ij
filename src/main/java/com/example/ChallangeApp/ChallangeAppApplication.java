ebaaf6533881c821b350496cde87bda6a72b7a09
Abddev-rqb <abdulch530@gmail.com>
2025-01-14 21:04:37 +0530

    Stable setups

---
+++ src/main/java/com/example/ChallangeApp/ChallangeAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.ChallangeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallangeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallangeAppApplication.class, args);
+	}
+}
